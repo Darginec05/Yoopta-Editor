ca1a5f5c098d10db3526f2747eb7f4b18a3d80be
Darginec05 <devopsbanda@gmail.com>
2023-05-05 17:31:15 +0200

    create demo

--- web/src/pages/index.tsx
+++ web/src/pages/index.tsx
@@ -1,29 +1,56 @@
-import Image from 'next/image';
-import YooptaEditor from '@yoopta/editor';
-import Paragraph from '@yoopta/paragraph';
+import YooptaEditor, { RenderElementProps } from '@yoopta/editor';
+import Paragraph, { ParagraphElement } from '@yoopta/paragraph';
 import Blockquote from '@yoopta/blockquote';
+import { NumberedList, BulletedList, TodoList } from '@yoopta/lists';
 import { Bold, Italic, CodeMark, Underline, Strike } from '@yoopta/marks';
 import { Inter } from 'next/font/google';
 import { useState } from 'react';
 import { Descendant } from 'slate';
+import { HeadingOne, HeadingThree, HeadingTwo } from '@yoopta/headings';
 
 const inter = Inter({ subsets: ['latin'] });
+
+const MyCustomParagraph = (props: RenderElementProps<ParagraphElement>) => {
+  const { children, attributes } = props;
+
+  return (
+    <p draggable={false} style={{ fontSize: 18, color: 'green' }} {...attributes}>
+      {children}
+    </p>
+  );
+};
+
+const plugins = [
+  Paragraph.extend({
+    shortcut: 'text',
+    renderer: () => MyCustomParagraph,
+  }),
+  Blockquote,
+  HeadingOne,
+  HeadingTwo,
+  HeadingThree,
+  NumberedList,
+  BulletedList,
+  TodoList,
+];
+
+const marks = [Bold, Italic, CodeMark, Underline, Strike];
 
 export default function Home() {
   const [editorValue, setEditorValue] = useState<Descendant[]>([]);
 
-  const marks = [Bold, Italic, CodeMark, Underline, Strike];
-
   return (
-    <main className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}>
-      <YooptaEditor
-        value={editorValue}
-        onChange={(data) => setEditorValue(data)}
-        plugins={[Paragraph, Blockquote]}
-        placeholder="Start typing..."
-        marks={marks}
-        autoFocus={false}
-      />
+    <main className={`flex min-h-screen w-full h-full flex-col items-center justify-between p-24 ${inter.className}`}>
+      <div className="w-full h-full">
+        <YooptaEditor
+          value={editorValue}
+          onChange={(val: Descendant[]) => setEditorValue(val)}
+          plugins={plugins}
+          marks={marks}
+          placeholder="Start typing..."
+          autoFocus
+        />
+      </div>
     </main>
   );
 }
