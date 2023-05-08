3d07afee9c259e4b199aa26efda11a53d5a87d76
Darginec05 <devopsbanda@gmail.com>
2023-05-08 20:48:49 +0200

    bump patch version

--- web/src/components/Toolbars/MediumToolbar.tsx
+++ web/src/components/Toolbars/MediumToolbar.tsx
@@ -1,11 +1,17 @@
+import { useElements, useMarks } from '@yoopta/editor';
 import { ToolbarProps } from '@yoopta/toolbar';
 import BoldIcon from './icons/medium/Bold.svg';
+import BlockquoteIcon from './icons/medium/Blockquote.svg';
 import ItalicIcon from './icons/medium/Italic.svg';
+import TitleIcon from './icons/medium/Title.svg';
+import SubtitleIcon from './icons/medium/Subtitle.svg';
 import LinkIcon from './icons/medium/Link.svg';
 import s from './MediumToolbar.module.scss';
 
 const MediumToolbar = (props: ToolbarProps) => {
-  const { getRootProps, marks } = props;
+  const { getRootProps } = props;
+  const marks = useMarks();
+  const elements = useElements();
 
   return (
     <div {...getRootProps()}>
@@ -20,32 +26,32 @@ const MediumToolbar = (props: ToolbarProps) => {
             <ItalicIcon />
           </span>
         </button>
-        <button type="button" className={s.button} onClick={() => marks.underline.toggle()}>
-          <span style={marks.underline.isActive ? { color: '#b5e5a4' } : { color: '#fff' }} className={s.item}>
-            <u>U</u>
-          </span>
-        </button>
-        <button type="button" className={s.button} onClick={() => marks.strike.toggle()}>
-          <span style={marks.strike.isActive ? { color: '#b5e5a4' } : { color: '#fff' }} className={s.item}>
-            <s>S</s>
-          </span>
-        </button>
-        <button type="button" className={s.button} onClick={() => marks.code.toggle()}>
-          <span style={marks.code.isActive ? { color: '#b5e5a4' } : { color: '#fff' }} className={s.item}>
-            <pre>{'</>'}</pre>
-          </span>
-        </button>
-        {/* <div className={s.separator} />
-        <button type="button" className={s.button} onClick={() => marks.bold.toggle()}>
-          <span style={marks.bold.isActive ? { color: '#b5e5a4' } : { color: '#fff' }} className={s.item}>
-            <ItalicIcon />
-          </span>
-        </button>
-        <button type="button" className={s.button} onClick={() => marks.bold.toggle()}>
+        {/* <button type="button" className={s.button} onClick={() => marks.bold.toggle()}>
           <span style={marks.bold.isActive ? { color: '#b5e5a4' } : { color: '#fff' }} className={s.item}>
             <LinkIcon />
           </span>
         </button> */}
+        <button type="button" className={s.button} onClick={() => marks.code.toggle()}>
+          <span style={marks.code.isActive ? { color: '#b5e5a4' } : { color: '#fff' }} className={s.item}>
+            <LinkIcon />
+          </span>
+        </button>
+        <div className={s.separator} />
+        <button type="button" className={s.button} onClick={() => elements['heading-one'].toggle()}>
+          <span style={{ color: '#fff' }} className={s.item}>
+            <TitleIcon />
+          </span>
+        </button>
+        <button type="button" className={s.button} onClick={() => elements['heading-two'].toggle()}>
+          <span style={{ color: '#fff' }} className={s.item}>
+            <SubtitleIcon />
+          </span>
+        </button>
+        <button type="button" className={s.button} onClick={() => elements.blockquote.toggle()}>
+          <span style={{ color: '#fff' }} className={s.item}>
+            <BlockquoteIcon />
+          </span>
+        </button>
       </div>
     </div>
   );
