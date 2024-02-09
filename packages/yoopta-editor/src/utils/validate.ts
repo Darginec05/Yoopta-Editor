f8918efff2602defaa8543e067c1f4210800ef41
Darginec05 <devopsbanda@gmail.com>
2024-02-10 00:36:39 +0100

    remove unused code

--- packages/yoopta-editor/src/utils/validate.ts
+++
@@ -1,7 +0,0 @@
-// WORK IN PROGRESS
-export function isValidYooptaNodes(nodes: any): boolean {
-  if (!Array.isArray(nodes)) return false;
-  if (nodes.length === 0) return false;
-
-  return true;
-}
