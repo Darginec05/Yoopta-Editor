62c4009c060438af3f228ef22ce7955379cac96f
Darginec05 <devopsbanda@gmail.com>
2024-03-16 00:34:38 +0100

    link tool is in progress

---
+++ packages/link-tool/src/components/DefaultLinkToolRender.tsx
@@ -0,0 +1,10 @@
+const DefaultLinkToolRender = () => {
+  return (
+    <div className="">
+      \asd
+      <input type="text" placeholder="Paste or type a link" className="yoo-link-tool-input yoo-link-tool-input-text" />
+    </div>
+  );
+};
+
+export { DefaultLinkToolRender };
