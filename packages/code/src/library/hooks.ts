782da73b4c5653cfb5ca3b02b096386660d99507
Darginec05 <devopsbanda@gmail.com>
2024-03-23 17:11:38 +0100

    code build fix

--- packages/code/src/library/hooks.ts
+++ packages/code/src/library/hooks.ts
@@ -1,1 +1,169 @@
-export {};
+import { useEffect, useState } from 'react';
+import { Annotation, EditorState, StateEffect, type Extension } from '@codemirror/state';
+import { EditorView, type ViewUpdate } from '@codemirror/view';
+import { getDefaultExtensions } from './extenstions';
+import { ReactCodeMirrorProps } from './types';
+
+const External = Annotation.define<boolean>();
+
+export interface UseCodeMirror extends ReactCodeMirrorProps {
+  container?: HTMLDivElement | null;
+}
+
+const emptyExtensions: Extension[] = [];
+
+export function useCodeMirror(props: UseCodeMirror) {
+  const {
+    value,
+    selection,
+    onChange,
+    onCreateEditor,
+    onUpdate,
+    extensions = emptyExtensions,
+    autoFocus,
+    theme = 'light',
+    height = null,
+    minHeight = null,
+    maxHeight = null,
+    width = null,
+    minWidth = null,
+    maxWidth = null,
+    placeholder: placeholderStr = '',
+    editable = true,
+    readOnly = false,
+    indentWithTab: defaultIndentWithTab = true,
+    basicSetup: defaultBasicSetup = true,
+    root,
+    initialState,
+  } = props;
+  const [container, setContainer] = useState<HTMLDivElement | null>();
+  const [view, setView] = useState<EditorView>();
+  const [state, setState] = useState<EditorState>();
+  const defaultThemeOption = EditorView.theme({
+    '&': {
+      height,
+      minHeight,
+      maxHeight,
+      width,
+      minWidth,
+      maxWidth,
+    },
+    '& .cm-scroller': {
+      height: '100% !important',
+    },
+  });
+  const updateListener = EditorView.updateListener.of((vu: ViewUpdate) => {
+    if (
+      vu.docChanged &&
+      typeof onChange === 'function' &&
+      // Fix echoing of the remote changes:
+      // If transaction is market as remote we don't have to call `onChange` handler again
+      !vu.transactions.some((tr) => tr.annotation(External))
+    ) {
+      const doc = vu.state.doc;
+      const value = doc.toString();
+      onChange(value, vu);
+    }
+  });
+
+  const defaultExtensions = getDefaultExtensions({
+    theme,
+    editable,
+    readOnly,
+    placeholder: placeholderStr,
+    indentWithTab: defaultIndentWithTab,
+    basicSetup: defaultBasicSetup,
+  });
+
+  let getExtensions = [updateListener, defaultThemeOption, ...defaultExtensions];
+
+  if (onUpdate && typeof onUpdate === 'function') {
+    getExtensions.push(EditorView.updateListener.of(onUpdate));
+  }
+  getExtensions = getExtensions.concat(extensions);
+
+  useEffect(() => {
+    if (container && !state) {
+      const config = {
+        doc: value,
+        selection,
+        extensions: getExtensions,
+      };
+      const stateCurrent = initialState
+        ? EditorState.fromJSON(initialState.json, config, initialState.fields)
+        : EditorState.create(config);
+      setState(stateCurrent);
+      if (!view) {
+        const viewCurrent = new EditorView({
+          state: stateCurrent,
+          parent: container,
+          root,
+        });
+        setView(viewCurrent);
+        onCreateEditor && onCreateEditor(viewCurrent, stateCurrent);
+      }
+    }
+    return () => {
+      if (view) {
+        setState(undefined);
+        setView(undefined);
+      }
+    };
+  }, [container, state]);
+
+  useEffect(() => setContainer(props.container), [props.container]);
+
+  useEffect(
+    () => () => {
+      if (view) {
+        view.destroy();
+        setView(undefined);
+      }
+    },
+    [view],
+  );
+
+  useEffect(() => {
+    if (autoFocus && view) {
+      view.focus();
+    }
+  }, [autoFocus, view]);
+
+  useEffect(() => {
+    if (view) {
+      view.dispatch({ effects: StateEffect.reconfigure.of(getExtensions) });
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [
+    theme,
+    extensions,
+    height,
+    minHeight,
+    maxHeight,
+    width,
+    minWidth,
+    maxWidth,
+    placeholderStr,
+    editable,
+    readOnly,
+    defaultIndentWithTab,
+    defaultBasicSetup,
+    onChange,
+    onUpdate,
+  ]);
+
+  useEffect(() => {
+    if (value === undefined) {
+      return;
+    }
+    const currentValue = view ? view.state.doc.toString() : '';
+    if (view && value !== currentValue) {
+      view.dispatch({
+        changes: { from: 0, to: currentValue.length, insert: value || '' },
+        annotations: [External.of(true)],
+      });
+    }
+  }, [value, view]);
+
+  return { state, setState, view, setView, container, setContainer };
+}
