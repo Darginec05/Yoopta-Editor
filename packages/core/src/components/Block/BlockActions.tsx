35d11b8651ac4512eb16b8a9db84059cd145122e
Darginec05 <devopsbanda@gmail.com>
2024-03-29 17:00:14 +0100

    remove all scss files

--- packages/core/src/components/Block/BlockActions.tsx
+++ packages/core/src/components/Block/BlockActions.tsx
@@ -9,7 +9,6 @@ import { Transforms } from 'slate';
 import { useState } from 'react';
 import { useFloating, offset, flip, inline, shift, useTransitionStyles } from '@floating-ui/react';
 import { BlockOptions } from '../../UI/BlockOptions/BlockOptions';
-import s from './Block.module.scss';
 
 type ActionsProps = {
   block: YooptaBlockData;
@@ -77,20 +76,24 @@ const BlockActions = ({ block, editor, dragHandleProps, showActions, onChangeAct
     refs.setReference(node);
   };
 
-  const style = { ...floatingStyles, ...blockOptionsTransitionStyles };
+  const blockOptionsFloatingStyle = { ...floatingStyles, ...blockOptionsTransitionStyles };
 
   return (
-    <div contentEditable={false} className={cx(s.actions, showActions && s.hovered, 'yoopta-element-actions')}>
+    <div
+      contentEditable={false}
+      data-hovered-state={showActions}
+      className={`yoo-editor-flex yoo-editor-absolute -yoo-editor-left-[50px] yoo-editor-top-[2px] yoo-editor-opacity-0 yoo-editor-transition-opacity yoopta-element-actions data-[hovered-state="true"]:yoo-editor-opacity-100`}
+    >
       <button
         type="button"
         onClick={onPlusClick}
-        className={cx(s.actionButton, s.plusButton, 'yoopta-element-actions-plus')}
+        className="yoo-editor-cursor-pointer yoo-editor-rounded-[6px] yoo-editor-w-auto yoo-editor-h-[24px] yoo-editor-flex yoo-editor-items-center yoo-editor-justify-center yoo-editor-bg-inherit yoo-editor-bg-transparent yoo-editor-transition-colors yoo-editor-duration-[180ms] yoo-editor-ease-[cubic-bezier(0.4,0,0.2,1)] yoo-editor-relative yoo-editor-w-[18px] yoo-editor-p-0 yoo-editor-text-[rgba(55,53,47,0.35)] yoo-editor-m-[0_1px] hover:yoo-editor-bg-[rgba(55,54,47,0.08)] focus:yoo-editor-bg-[rgba(55,54,47,0.08)] active:yoo-editor-bg-[rgba(55,54,47,0.08)] yoo-editor-w-[24px] yoopta-element-actions-plus"
       >
         <PlusIcon />
       </button>
       <button
         type="button"
-        className={cx(s.actionButton, 'yoopta-element-actions-drag')}
+        className="yoo-editor-cursor-pointer yoo-editor-rounded-[6px] yoo-editor-w-auto yoo-editor-h-[24px] yoo-editor-flex yoo-editor-items-center yoo-editor-justify-center yoo-editor-bg-inherit yoo-editor-bg-transparent yoo-editor-transition-colors yoo-editor-duration-[180ms] yoo-editor-ease-[cubic-bezier(0.4,0,0.2,1)] yoo-editor-relative yoo-editor-w-[18px] yoo-editor-p-0 yoo-editor-text-[rgba(55,53,47,0.35)] yoo-editor-m-[0_1px] hover:yoo-editor-bg-[rgba(55,54,47,0.08)] focus:yoo-editor-bg-[rgba(55,54,47,0.08)] active:yoo-editor-bg-[rgba(55,54,47,0.08)] yoopta-element-actions-drag"
         ref={onDragButtonRef}
         {...attributes}
         {...listeners}
@@ -98,7 +101,12 @@ const BlockActions = ({ block, editor, dragHandleProps, showActions, onChangeAct
       >
         <DragIcon />
       </button>
-      <BlockOptions isOpen={isMounted} refs={refs} style={style} onClose={() => setIsBlockOptionsOpen(false)} />
+      <BlockOptions
+        isOpen={isMounted}
+        refs={refs}
+        style={blockOptionsFloatingStyle}
+        onClose={() => setIsBlockOptionsOpen(false)}
+      />
     </div>
   );
 };
