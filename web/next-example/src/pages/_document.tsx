905853549cf60c89d308ea3128bea1b152cc08b7
Darginec05 <devopsbanda@gmail.com>
2024-03-31 21:41:27 +0200

    prepare examples

--- web/next-example/src/pages/_document.tsx
+++ web/next-example/src/pages/_document.tsx
@@ -1,9 +1,11 @@
-import { Html, Head, Main, NextScript } from "next/document";
+import { Html, Head, Main, NextScript } from 'next/document';
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <title>Yoopta-Editor v4</title>
+      </Head>
       <body>
         <Main />
         <NextScript />
