58275a641929f59d77b5e5b6ca5c7d860fb0e674
Darginec05 <devopsbanda@gmail.com>
2024-04-14 02:23:36 +0300

    change path for base example

--- web/next-example-tailwind/src/pages/index.tsx
+++ web/next-example-tailwind/src/pages/index.tsx
@@ -5,7 +5,7 @@ export default function Home() {
   const router = useRouter();
 
   useEffect(() => {
-    router.push('/examples/withBasicExample');
+    router.push('/examples/withBaseFullSetup');
   });
 
   return null;
