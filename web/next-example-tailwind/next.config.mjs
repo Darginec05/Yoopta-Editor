91137dd662a74f84db4914f83f03aa89a1448de0
Darginec05 <devopsbanda@gmail.com>
2024-04-14 23:01:34 +0300

    trigger redebploy

--- web/next-example-tailwind/next.config.mjs
+++ web/next-example-tailwind/next.config.mjs
@@ -4,6 +4,9 @@ const nextConfig = {
   typescript: {
     ignoreBuildErrors: true,
   },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
   webpack(config) {
     const fileLoaderRule = config.module.rules.find((rule) => rule.test?.test?.('.svg'));
 
