e251115e1861d33f1940425b134d0e507d750c3f
Darginec05 <devopsbanda@gmail.com>
2024-04-14 22:58:56 +0300

    trigger redebploy

--- web/next-example-tailwind/src/components/examples/withChatSlack.tsx
+++ web/next-example-tailwind/src/components/examples/withChatSlack.tsx
@@ -4,7 +4,7 @@ function WithSlackChatExample() {
   return (
     <div className="md:p-[80px] px-[20px] pt-[80px] pb-[40px] flex flex-col items-center justify-center">
       <div className="mb-8">
-        <h1 className="mt-6 scroll-m-20 text-4xl font-bold tracking-tight lg:text-3x undefined">
+        <h1 className="mt-6 scroll-m-20 text-4xl font-bold tracking-tight lg:text-3x">
           You can even create chats using Yoopta-Editor!
         </h1>
         <p className="text-xl mt-2">Let's check example for slack chat</p>
