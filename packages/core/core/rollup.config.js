2e623d8c4b3cec67c3e4eb3936fcf1106201f02f
Andrew Red <andrew@reddikh.com>
2024-05-15 20:53:27 +0100

    - changed structure of packages to separate folders

---
+++ packages/core/core/rollup.config.js
@@ -0,0 +1,7 @@
+import { createRollupConfig } from '../../../config/rollup';
+
+const pkg = require('./package.json');
+export default createRollupConfig({
+  pkg,
+  tailwindConfig: { content: ['./src/**/*.{js,ts,jsx,tsx,mdx}'], prefix: 'yoo-editor-' },
+});
