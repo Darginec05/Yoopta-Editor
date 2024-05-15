f6e5d693e60812b792fbe1ce4acd5075d3f2cc91
Darginec05 <devopsbanda@gmail.com>
2024-05-15 19:11:12 +0300

    fixes after review

--- packages/blockquote/src/ui/Blockquote.tsx
+++ packages/blockquote/src/ui/Blockquote.tsx
@@ -1,11 +1,5 @@
 import { PluginElementRenderProps } from '@yoopta/editor';
 
-// const blockquoteStyles = {
-//   margin: '8px 0 0 0',
-//   border: 'none',
-//   borderLeft: '2px solid #e5e7eb',
-// };
-
 const BlockquoteRender = (props: PluginElementRenderProps) => {
   const { className = '', ...htmlAttrs } = props.HTMLAttributes || {};
 
