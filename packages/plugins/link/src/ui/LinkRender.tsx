8129c67510c9bdc6a1bafc6c9bc608c2f8840ecd
Darginec05 <devopsbanda@gmail.com>
2024-05-18 17:23:40 +0300

    link tool UX is improved with additional props

--- packages/plugins/link/src/ui/LinkRender.tsx
+++ packages/plugins/link/src/ui/LinkRender.tsx
@@ -1,9 +1,12 @@
 import { PluginElementRenderProps, useYooptaReadOnly } from '@yoopta/editor';
+import { LinkElementProps } from '../types';
+
+const VALID_TARGET_VALUES = ['_blank', '_self', '_parent', '_top', 'framename'];
 
 const LinkRender = (props: PluginElementRenderProps) => {
   const { className = '', ...htmlAttrs } = props.HTMLAttributes || {};
 
-  const { url, target, rel } = props.element.props || {};
+  const { url, target = '', rel } = props.element.props || {};
   const isReadOnly = useYooptaReadOnly();
 
   const onClick = (e) => {
@@ -11,15 +14,27 @@ const LinkRender = (props: PluginElementRenderProps) => {
     e.preventDefault();
   };
 
+  const linkProps: Partial<Pick<LinkElementProps, 'rel' | 'target'>> = {
+    rel,
+    target,
+  };
+
+  if (!VALID_TARGET_VALUES.includes(target)) {
+    delete linkProps.target;
+  }
+
+  if (typeof rel !== 'string' || rel?.length === 0) {
+    delete linkProps.rel;
+  }
+
   return (
     <a
       data-element-type={props.element.type}
       draggable={false}
       href={url || ''}
-      rel={rel}
-      target={target}
       onClick={onClick}
       className={`yoopta-link ${className}`}
+      {...linkProps}
       {...htmlAttrs}
       {...props.attributes}
     >
