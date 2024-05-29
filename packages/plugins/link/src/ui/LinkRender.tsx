ade3f7063279101ff31735971aa3c1326cbce75c
Darginec05 <devopsbanda@gmail.com>
2024-05-29 17:35:59 +0300

    added preview link

--- packages/plugins/link/src/ui/LinkRender.tsx
+++ packages/plugins/link/src/ui/LinkRender.tsx
@@ -1,18 +1,40 @@
-import { PluginElementRenderProps, useYooptaReadOnly } from '@yoopta/editor';
+import { PluginElementRenderProps, useYooptaReadOnly, useYooptaTools, UI } from '@yoopta/editor';
+import { useState } from 'react';
 import { LinkElementProps } from '../types';
+import { LinkHoverPreview } from './LinkHoverPreview';
+import { useFloating, offset, flip, shift, inline, autoUpdate, useTransitionStyles } from '@floating-ui/react';
+import { SquareArrowOutUpRight } from 'lucide-react';
 
 const VALID_TARGET_VALUES = ['_blank', '_self', '_parent', '_top', 'framename'];
 
 const LinkRender = ({ extendRender, ...props }: PluginElementRenderProps) => {
+  const [hovered, setHovered] = useState(false);
+
   const { className = '', ...htmlAttrs } = props.HTMLAttributes || {};
+
+  const {
+    refs: linkPreviewRefs,
+    floatingStyles: linkPreviewFloatingStyles,
+    context,
+  } = useFloating({
+    placement: 'bottom',
+    open: hovered,
+    onOpenChange: setHovered,
+    middleware: [inline(), flip(), shift(), offset(0)],
+    whileElementsMounted: autoUpdate,
+  });
+
+  const { isMounted: isActionMenuMounted, styles: linkPreviewTransitionStyles } = useTransitionStyles(context, {
+    duration: {
+      open: 100,
+      close: 100,
+    },
+  });
+
+  const linkPreviewStyles = { ...linkPreviewFloatingStyles, ...linkPreviewTransitionStyles };
 
   const { url, target = '', rel } = props.element.props || {};
   const isReadOnly = useYooptaReadOnly();
-
-  const onClick = (e) => {
-    if (isReadOnly) return;
-    e.preventDefault();
-  };
 
   const linkProps: Partial<Pick<LinkElementProps, 'rel' | 'target'>> = {
     rel,
@@ -30,6 +52,24 @@ const LinkRender = ({ extendRender, ...props }: PluginElementRenderProps) => {
   if (extendRender) {
     return extendRender(props);
   }
+  const onMouseOver = () => {
+    console.log('onMouseOver', onMouseOver);
+    setHovered(true);
+  };
+  const onMouseOut = () => {
+    console.log('onMouseOut', onMouseOut);
+    setHovered(false);
+  };
+
+  const onRef = (ref) => {
+    props.attributes.ref = ref;
+    linkPreviewRefs.setReference(ref);
+  };
+
+  const onClick = (e) => {
+    // if (isReadOnly) return;
+    // e.preventDefault();
+  };
 
   return (
     <a
@@ -37,11 +77,18 @@ const LinkRender = ({ extendRender, ...props }: PluginElementRenderProps) => {
       href={url || ''}
       onClick={onClick}
       className={`yoopta-link ${className}`}
+      onMouseOver={onMouseOver}
+      onMouseOut={onMouseOut}
       {...linkProps}
       {...htmlAttrs}
       {...props.attributes}
+      ref={onRef}
     >
       {props.children}
+
+      {isActionMenuMounted && !isReadOnly && (
+        <LinkHoverPreview style={linkPreviewStyles} setFloating={linkPreviewRefs.setFloating} element={props.element} />
+      )}
     </a>
   );
 };
