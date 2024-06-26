b70a6ceeb804ebbc93920138991204fe6d0c0c05
Darginec05 <devopsbanda@gmail.com>
2024-06-26 23:04:16 +0200

    fix number list ordering

--- packages/core/exports/src/markdown/deserialize.ts
+++ packages/core/exports/src/markdown/deserialize.ts
@@ -3,8 +3,6 @@ import { marked } from 'marked';
 import { deserializeHTML } from '../html/deserialize';
 
 export function deserializeMarkdown(editor: YooEditor, markdown: string): YooptaContentValue {
-  const html = marked.parse(markdown, { gfm: true, breaks: true, pedantic: false });
-  console.log('html from marked', html);
-
+  const html = marked.parse(markdown, { gfm: true, breaks: true, pedantic: false }) as string;
   return deserializeHTML(editor, html);
 }
