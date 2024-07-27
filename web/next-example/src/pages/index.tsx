b84400ad8eba57a2d0140c76ce2ad3d8f60b45b3
Darginec05 <devopsbanda@gmail.com>
2024-07-27 21:25:02 +0200

    update packages

--- web/next-example/src/pages/index.tsx
+++ web/next-example/src/pages/index.tsx
@@ -55,13 +55,12 @@ const GithubButton = ({ repo }) => {
 const ProductHuntButton = () => {
   return (
     <a
-      href="https://www.producthunt.com/posts/yoopta-editor?embed=true&utm_source=badge-featured&utm_medium=badge&utm_souce=badge-yoopta&#0045;editor"
+      href="https://www.producthunt.com/posts/yoopta-editor?embed=true&utm_source=badge-top-post-badge&utm_medium=badge&utm_souce=badge-yoopta&#0045;editor"
       target="_blank"
     >
       <img
-        src="https://api.producthunt.com/widgets/embed-image/v1/featured.svg?post_id=453627&theme=light"
-        alt="Yoopta Editor - The truly open-source rich-text editor | Product Hunt"
-        // style="width: 250px; height: 54px;"
+        src="https://api.producthunt.com/widgets/embed-image/v1/top-post-badge.svg?post_id=453627&theme=light&period=daily"
+        alt="Yoopta&#0045;Editor - Open&#0045;source&#0032;text&#0045;editor | Product Hunt"
         width="250"
         height="36"
         className="w-52 h-9 mt-2 md:mt-0"
