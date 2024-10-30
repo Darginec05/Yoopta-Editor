43abc2970c2044a37c2e7c74f0853c31f0c52f63
Akhmed Ibragimov <devopsbanda@gmail.com>
2024-10-30 23:11:59 +0100

    Fixes triggering slash commands (#363)
    * fixes for trigger slash commands

--- tests/test-utils.tsx
+++ tests/test-utils.tsx
@@ -22,10 +22,17 @@ export const DefaultParagraph = new YooptaPlugin({
       props: { nodeType: 'block' },
     },
   },
+  options: {
+    shortcuts: ['p', 'text', 'para'],
+    display: {
+      title: 'Paragraph',
+      description: 'Paragraph block',
+    },
+  },
 });
 
 export const BlockPluginWithProps = new YooptaPlugin({
-  type: 'Block',
+  type: 'BlockPluginWithProps',
   elements: {
     block: {
       render: (props) => <div {...props.attributes}>{props.children}</div>,
@@ -56,28 +63,85 @@ const PluginWithSeveralElements = new YooptaPlugin({
 
 export const TEST_PLUGIN_LIST = [InlinePlugin, DefaultParagraph, BlockPluginWithProps, PluginWithSeveralElements];
 
-export function renderYooptaEditor(props: Partial<YooptaEditorProps> = {}) {
-  const editor = createYooptaEditor();
-
-  return render(
-    <YooptaEditor {...props} editor={props.editor || editor} plugins={props.plugins || TEST_PLUGIN_LIST} />,
-  );
-}
-
-const data = {
+const DEFAULT_EDITOR_STATE = {
   'aafd7597-1e9a-4c80-ab6c-88786595d72a': {
-    'aafd7597-1e9a-4c80-ab6c-88786595d72a': {
-      id: 'aafd7597-1e9a-4c80-ab6c-88786595d72a',
-      meta: { depth: 0, order: 0 },
-      type: 'Paragraph',
-      value: [
-        {
-          id: '3aff9e2c-5fee-43ff-b426-1e4fee8b303c',
-          type: 'paragraph',
-          props: { nodeType: 'block' },
-          children: [{ text: '' }],
+    id: 'aafd7597-1e9a-4c80-ab6c-88786595d72a',
+    meta: { depth: 0, order: 0 },
+    type: 'Paragraph',
+    value: [
+      {
+        id: '3aff9e2c-5fee-43ff-b426-1e4fee8b303c',
+        type: 'paragraph',
+        props: { nodeType: 'block' },
+        children: [{ text: '' }],
+      },
+    ],
+  },
+  '7025b887-a746-42dd-9d37-56253dce4e96': {
+    id: '7025b887-a746-42dd-9d37-56253dce4e96',
+    type: 'Paragraph',
+    value: [
+      {
+        id: 'e46ac801-0bc2-455a-9138-ef5e141e4845',
+        type: 'paragraph',
+        children: [
+          {
+            text: 'Read without limits or ads, fund great writers, and join a community that believes in human storytelling with',
+            italic: true,
+            bold: true,
+          },
+          {
+            text: ' ',
+            italic: true,
+            bold: true,
+          },
+          {
+            id: '19554e2b-180a-461e-b287-98416f7f30d4',
+            type: 'link',
+            props: {
+              url: 'https://medium.com/membership',
+              target: '_blank',
+              rel: 'noopener',
+              title: 'membership',
+              nodeType: 'inline',
+            },
+            children: [
+              {
+                bold: true,
+                italic: true,
+                text: 'membership',
+              },
+            ],
+          },
+          {
+            text: '.',
+            italic: true,
+            bold: true,
+          },
+        ],
+        props: {
+          nodeType: 'block',
         },
-      ],
+      },
+    ],
+    meta: {
+      align: 'left',
+      depth: 0,
+      order: 1,
     },
   },
 };
+
+export function renderYooptaEditor(props: Partial<YooptaEditorProps> = {}) {
+  const editor = props.editor || createYooptaEditor();
+
+  return render(
+    <YooptaEditor
+      {...props}
+      value={props.value || DEFAULT_EDITOR_STATE}
+      editor={props.editor || editor}
+      plugins={props.plugins || TEST_PLUGIN_LIST}
+      autoFocus={props.autoFocus || false}
+    />,
+  );
+}
